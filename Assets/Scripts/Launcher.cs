d5f3725f99034125a9c27837edefef94ca7d547f
Sergi Maymi <smaymi@portic.net>
2020-07-15 18:22:33 +0200

    get random word, shuffle and places tiles

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,79 +1,101 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
+
 
 public class Launcher : MonoBehaviour
 {
     private WordDB db;
 
     public GameObject[] aTiles;
-    public Dictionary<string, GameObject> tiles = new Dictionary<string, GameObject>();
+    public Dictionary<char, GameObject> tiles = new Dictionary<char, GameObject>();
     public string a;
 
     // Start is called before the first frame update
     void Start()
     {
-        tiles.Add("A", aTiles[0]);
-        tiles.Add("B", aTiles[1]);
-        tiles.Add("C", aTiles[2]);
-        tiles.Add("D", aTiles[3]);
-        tiles.Add("E", aTiles[4]);
-        tiles.Add("F", aTiles[5]);
-        tiles.Add("G", aTiles[6]);
-        tiles.Add("H", aTiles[7]);
-        tiles.Add("I", aTiles[8]);
-        tiles.Add("J", aTiles[9]);
-        tiles.Add("K", aTiles[10]);
-        tiles.Add("L", aTiles[11]);
-        tiles.Add("M", aTiles[12]);
-        tiles.Add("N", aTiles[13]);
-        tiles.Add("O", aTiles[14]);
-        tiles.Add("P", aTiles[15]);
-        tiles.Add("Q", aTiles[16]);
-        tiles.Add("R", aTiles[17]);
-        tiles.Add("S", aTiles[18]);
-        tiles.Add("T", aTiles[19]);
-        tiles.Add("U", aTiles[20]);
-        tiles.Add("V", aTiles[21]);
-        tiles.Add("X", aTiles[22]);
-        tiles.Add("Z", aTiles[23]);
-        tiles.Add("Ç", aTiles[24]);
-        tiles.Add("2", aTiles[25]); // L·L
-        tiles.Add("1", aTiles[26]); // NY
+        tiles.Add('A', aTiles[0]);
+        tiles.Add('B', aTiles[1]);
+        tiles.Add('C', aTiles[2]);
+        tiles.Add('D', aTiles[3]);
+        tiles.Add('E', aTiles[4]);
+        tiles.Add('F', aTiles[5]);
+        tiles.Add('G', aTiles[6]);
+        tiles.Add('H', aTiles[7]);
+        tiles.Add('I', aTiles[8]);
+        tiles.Add('J', aTiles[9]);
+        tiles.Add('K', aTiles[10]);
+        tiles.Add('L', aTiles[11]);
+        tiles.Add('M', aTiles[12]);
+        tiles.Add('N', aTiles[13]);
+        tiles.Add('O', aTiles[14]);
+        tiles.Add('P', aTiles[15]);
+        tiles.Add('Q', aTiles[16]);
+        tiles.Add('R', aTiles[17]);
+        tiles.Add('S', aTiles[18]);
+        tiles.Add('T', aTiles[19]);
+        tiles.Add('U', aTiles[20]);
+        tiles.Add('V', aTiles[21]);
+        tiles.Add('X', aTiles[22]);
+        tiles.Add('Z', aTiles[23]);
+        tiles.Add('Ç', aTiles[24]);
+        tiles.Add('2', aTiles[25]); // L·L
+        tiles.Add('1', aTiles[26]); // NY
 
-        Instantiate(tiles["A"], new Vector3(-5,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["B"], new Vector3(-4,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["C"], new Vector3(-3,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["D"], new Vector3(-2,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["E"], new Vector3(-1,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["F"], new Vector3(0,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["G"], new Vector3(1,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["H"], new Vector3(2,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["I"], new Vector3(3,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["J"], new Vector3(4,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["K"], new Vector3(5,3,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["L"], new Vector3(-5,2,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["M"], new Vector3(-3,2,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["N"], new Vector3(-4,2,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["O"], new Vector3(-2,2,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["P"], new Vector3(-1,2,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["Q"], new Vector3(0,2,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["R"], new Vector3(1,2,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["S"], new Vector3(2,2,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["T"], new Vector3(-5,1,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["U"], new Vector3(-4,1,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["V"], new Vector3(-3,1,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["X"], new Vector3(-2,1,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["Z"], new Vector3(-1,1,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["Ç"], new Vector3(0,1,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["1"], new Vector3(1,1,0), tiles["A"].transform.rotation);
-        Instantiate(tiles["2"], new Vector3(2,1,0), tiles["A"].transform.rotation);
+        Instantiate(tiles['A'], new Vector3(-5, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['B'], new Vector3(-4, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['C'], new Vector3(-3, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['D'], new Vector3(-2, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['E'], new Vector3(-1, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['F'], new Vector3(0, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['G'], new Vector3(1, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['H'], new Vector3(2, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['I'], new Vector3(3, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['J'], new Vector3(4, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['K'], new Vector3(5, 3, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['L'], new Vector3(-5, 2, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['M'], new Vector3(-3, 2, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['N'], new Vector3(-4, 2, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['O'], new Vector3(-2, 2, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['P'], new Vector3(-1, 2, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['Q'], new Vector3(0, 2, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['R'], new Vector3(1, 2, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['S'], new Vector3(2, 2, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['T'], new Vector3(-5, 1, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['U'], new Vector3(-4, 1, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['V'], new Vector3(-3, 1, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['X'], new Vector3(-2, 1, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['Z'], new Vector3(-1, 1, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['Ç'], new Vector3(0, 1, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['1'], new Vector3(1, 1, 0), tiles['A'].transform.rotation);
+        Instantiate(tiles['2'], new Vector3(2, 1, 0), tiles['A'].transform.rotation);
 
+
+        string word5 = RandomWord(db.words5);
+        List<char> datalist = new List<char>();
+        datalist.AddRange(word5);
+        int idx = -5;
+        datalist.ForEach(c => Instantiate(tiles[c], new Vector3(idx++, 0, 0), tiles['A'].transform.rotation));
+        //shuffle list
+        datalist = datalist.OrderBy(i => Guid.NewGuid()).ToList();
+        idx = -5;
+        datalist.ForEach(c => Instantiate(tiles[c], new Vector3(idx++, -1, 0), tiles['A'].transform.rotation));
+
+
+    }
+
+    private string RandomWord(List<string> list)
+    {
+        int max = list.Count-1;
+        int idx = UnityEngine.Random.Range(0, max);
+        return list[idx];
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         
     }
        
@@ -90,4 +112,5 @@ public class Launcher : MonoBehaviour
         Debug.Log("There are  "+db.words5.Count+" words of length 5");
         //Debug.Log("There are  "+db.words5.Count+" words of length 5");
     }
+
 }
